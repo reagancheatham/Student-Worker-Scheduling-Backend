8efc6f269c0a8a209f85deb5881b2b298c163313
Maximus253 <m.jones@eagles.oc.edu>
2026-02-10 18:03:44 -0600

    Add Permission Role

---
+++ src/models/permissionRole.model.ts
@@ -0,0 +1,45 @@
+import {
+    Model,
+    InferAttributes,
+    InferCreationAttributes,
+    CreationOptional,
+    DataTypes,
+} from "sequelize";
+import sequelizeInstance from "../database/sequelizeInstance";
+
+class PermissionRole extends Model<
+    InferAttributes<PermissionRole>,
+    InferCreationAttributes<PermissionRole>
+> {
+    declare id: CreationOptional<number>;
+    declare role: string;
+}
+
+PermissionRole.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize: sequelizeInstance,
+        tableName: "employee",
+        timestamps: false,
+        indexes: [
+            {
+                unique: true,
+                fields: [
+                    "role",
+                ],
+            },
+        ],
+    },
+);
+
+export default PermissionRole;
