8601610186654c56923938046c80febecfe386cd
reagancheatham <reagan.cheatham@eagles.oc.edu>
2026-02-25 10:28:25 -0600

    Create shiftTaskTemplate.model.ts

---
+++ src/models/shiftTaskTemplate.model.ts
@@ -0,0 +1,54 @@
+import { Model, DataTypes } from "sequelize";
+import type {
+    CreationOptional,
+    InferAttributes,
+    InferCreationAttributes,
+} from "sequelize";
+import { sequelizeInstance } from "../config/sequelizeInstance.ts";
+import { ShiftTaskListTemplate } from "./shiftTaskListTemplate.model.ts";
+
+export class ShiftTaskTemplate extends Model<
+    InferAttributes<ShiftTaskTemplate>,
+    InferCreationAttributes<ShiftTaskTemplate>
+> {
+    declare id: CreationOptional<number>;
+    declare shiftTaskListID: number;
+    declare name: string;
+    declare description: string;
+}
+
+ShiftTaskTemplate.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        shiftTaskListID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            references: {
+                model: ShiftTaskListTemplate,
+                key: "id",
+            },
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        sequelize: sequelizeInstance,
+        tableName: "scheduleTemplate",
+        timestamps: false,
+        indexes: [
+            {
+                unique: true,
+                fields: ["id", "shiftTaskListID", "name"],
+            },
+        ],
+    },
+);
