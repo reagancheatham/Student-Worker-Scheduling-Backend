1bd864d635e49e7523612b3e7fe7c8a2a3d52d7a
Maximus253 <122314662+Maximus253@users.noreply.github.com>
2026-04-06 13:05:10 -0500

    Mj add admin view (#76)
    * Eddited call functions
    
    * Change manager to businessPermissionRole
    
    * Admid edit and delete working
    
    * Added Invite Function
    
    * Added email invites
    
    * Capitalization Fix
    
    * Not Working, but pogress
    
    * Not completely fixed yet, but its a start
    
    * Fixed passing stuff
    
    * Update business.ts

--- src/models/taskListTemplate.ts
+++ src/models/taskListTemplate.ts
@@ -33,6 +33,7 @@ TaskListTemplate.init(
                 model: Business,
                 key: "id",
             },
+            onDelete: "CASCADE"
         },
         name: {
             type: DataTypes.STRING,
