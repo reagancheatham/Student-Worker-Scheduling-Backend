02614c6556108ca39b0eda2f2d27f198cce84854
reagancheatham <reagan.cheatham@eagles.oc.edu>
2026-04-07 21:56:40 -0500

    Add task list routes

--- src/models/taskList.ts
+++ src/models/taskList.ts
@@ -11,6 +11,7 @@ import { ModelRouter } from "../classes/databaseModel.ts";
 import { Router } from "express";
 import { ScheduleDatabase } from "../classes/scheduleDatabase.ts";
 import { Task } from "./task.ts";
+import { TaskStatus } from "../classes/taskStatus.ts";
 
 export class TaskList extends Model<
     InferAttributes<TaskList>,
@@ -61,22 +62,97 @@ class TaskListRouter extends ModelRouter {
     }
 
     protected buildRouter(router: Router): void {
-        router.post("/", (req, res) =>
-            ScheduleDatabase.create(TaskList, req, res),
-        );
-        router.put("/", (req, res) =>
-            ScheduleDatabase.update(TaskList, req, res, "id"),
-        );
+        router.post("/", TaskListRouter.createTaskList);
+        router.put("/", TaskListRouter.updateTaskList);
         router.delete("/:id", (req, res) =>
             ScheduleDatabase.delete(TaskList, req, res, "id"),
         );
         router.get("/:id", (req, res) =>
             ScheduleDatabase.get(TaskList, req, res, "id"),
         );
-        router.get("/shift/:shiftID", this.getOrCreateForShift);
+        router.get("/shift/:shiftID", TaskListRouter.getOrCreateForShift);
     }
 
-    private async getOrCreateForShift(req: Request, res: Response) {
+    private static async createTaskList(req: Request, res: Response) {
+        const info = req.body;
+
+        if (!info) {
+            console.error(`Error updating ${TaskList.name}: info is null`);
+            return Promise.resolve();
+        }
+
+        console.log(
+            `Creating ${TaskList.name} with info: ${JSON.stringify(info)}`,
+        );
+
+        try {
+            const result = await TaskList.create(info);
+            const tasks = info.tasks as Task[];
+
+            tasks.forEach((task) => {
+                task.taskListID = result.id;
+            });
+
+            await TaskListRouter.updateTaskListTasks(tasks);
+        } catch (error) {
+            console.error(`Error creating ${TaskList.name}: ${error}`);
+            res.status(500).send({ error });
+        }
+    }
+
+    private static async updateTaskList(req: Request, res: Response) {
+        const info = req.body;
+
+        if (!info) {
+            console.error(`Error updating ${TaskList.name}: info is null`);
+            return Promise.resolve();
+        }
+
+        console.log(
+            `Updating ${TaskList.name} with info: ${JSON.stringify(info)}`,
+        );
+
+        const id = info.id;
+        const tasks = info.tasks as Task[];
+
+        try {
+            const result = await TaskList.update(info, {
+                where: {
+                    id,
+                },
+            });
+
+            if (result[0] === 0)
+                console.log(`Could not find a ${TaskList.name} to update`);
+            else console.log(`Updated ${result[0]} ${TaskList.name}s`);
+
+            await TaskListRouter.updateTaskListTasks(tasks);
+
+            res.status(200).send({ affectedCount: result[0] });
+        } catch (error) {
+            console.error(`Error updating ${TaskList.name}: ${error}`);
+            res.status(500).send({ error });
+        }
+    }
+
+    private static async updateTaskListTasks(tasks: Task[]) {
+        if (!tasks)
+            // No tasks to update, just leave
+            return Promise.resolve();
+
+        console.log(`Updating ${TaskList.name} tasks`);
+
+        const promises = tasks.map(async (task) => {
+            const id = task.id;
+
+            if (task.id > 0) await Task.update(task, { where: { id } });
+            else await Task.create(task);
+        });
+
+        await Promise.all(promises);
+    }
+
+    private static async getOrCreateForShift(req: Request, res: Response) {
         try {
             const response = await TaskList.findOrCreate({
                 where: req.params,
