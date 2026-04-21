ea9e5c4e373699b77fb8aa0c9eac9c28014391e8
James-Wiley <james.wiley@live.com>
2026-04-20 22:04:54 -0500

    revert server change

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ app.use(cors(defaultCorsConfig))
     .use(express.urlencoded({ extended: true }))
     .use("/workerscheduling-t6", router);
 
-const port = Number(process.env.NODE_PORT) || 3136;
+const port = Number(process.env.NODE_PORT);
 app.listen(port, () => {
     Logger.log(`Server is listening on port ${port}.`);
 });
